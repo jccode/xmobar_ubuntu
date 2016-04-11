8a032b5316cd33c65d4867eb31393ac3e2ede0ba
jcchen <junchangchen@gmail.com>
2016-04-11 11:55:16 +0800

    FNKeys

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,6 @@
 
 import qualified Data.Map                 as M
+import           Graphics.X11.ExtraTypes
 import           System.IO
 import           XMonad
 import           XMonad.Hooks.DynamicLog
@@ -31,20 +32,27 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) =
     [ ((controlMask .|. mod4Mask, xK_Delete), spawn "xscreensaver-command -lock")
     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
     , ((0, xK_Print), spawn "scrot")
+
+    -- 
+    , ((0, xF86XK_AudioLowerVolume),        spawn "pactl set-sink-volume 1 -1.5%")
+    , ((0, xF86XK_AudioRaiseVolume),        spawn "pactl set-sink-volume 1 +1.5%")
+    , ((0, xF86XK_AudioMute),        spawn "pactl set-sink-mute 1 toggle")
+      
     ]
 
 
 -- main
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-    xmonad $ defaults xmproc
+    xmonad $ defaults {
+        logHook            = myLogHook xmproc
+        }
 
-defaults xmproc = defaultConfig {
+defaults = defaultConfig {
     terminal           = myTerminal,
     modMask            = myModMask,
     focusedBorderColor = myFocusedBorderColor,
     layoutHook         = myLayout,
     manageHook         = myManageHook,
-    logHook            = myLogHook xmproc,
     keys               = myKeys
     }
